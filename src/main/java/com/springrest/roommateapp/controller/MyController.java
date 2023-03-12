15b6fd1babdb122472eb2df78e9170cc83589542
AAKASH26229 <parateakash21.ap@gmail.com>
2023-03-13 03:48:25 +0530

    created matching

--- src/main/java/com/springrest/roommateapp/controller/MyController.java
+++ src/main/java/com/springrest/roommateapp/controller/MyController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,8 +39,42 @@ public class MyController {
 	private UserService userService;
 
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+
+		model.addAttribute("name", "rohit");
+
 		return "index";
+	}
+
+	@GetMapping("/view-all-users")
+	public String viewAllUsers() {
+		return "admin-view-all-users";
+	}
+
+	@GetMapping("/matches")
+	public String viewAllMatches() {
+		return "my-matches";
+	}
+
+	@GetMapping("/view-a-user")
+	public String viewAUser(@RequestParam("user_id") String itemId, Model model) {
+		System.out.println(itemId);
+
+		model.addAttribute("userId", itemId);
+		return "admin-view-a-user";
+	}
+
+	@GetMapping("/view-all-rooms")
+	public String viewAllRooms() {
+		return "admin-view-all-rooms";
+	}
+
+	@GetMapping("/view-a-room")
+	public String viewARoom(@RequestParam("room_id") String itemId, Model model) {
+		System.out.println(itemId);
+
+		model.addAttribute("roomId", itemId);
+		return "admin-view-a-room";
 	}
 
 	@ModelAttribute("user")
@@ -56,7 +91,7 @@ public class MyController {
 	@PostMapping("/registration")
 	public String addUser(@ModelAttribute("user") Userdto userDto) {
 		userService.addUser(userDto);
-//		session.setAttribute("message", "User registerd successfully!!");
+		// session.setAttribute("message", "User registerd successfully!!");
 		return "redirect:/api/registration?success";
 	}
 
@@ -68,15 +103,22 @@ public class MyController {
 	@PostMapping("/login")
 	public String loginUser(ModelMap model, @RequestParam String email, @RequestParam String password,
 			HttpSession session) {
-		Userdto u = userService.getUserByEmailAndPass(email,password);  
+		Userdto u = userService.getUserByEmailAndPass(email, password);
 		if (u != null) {
 			session.setAttribute("Userdto", u);
+			System.out.println(session.getAttribute("Userdto"));
 			return "redirect:/api/home";
 		} else {
 			session.setAttribute("msg", "Oops...Something went wrong..Try again....");
 			return "redirect:/api/login";
 		}
 
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("Userdto");
+		return "redirect:/api/login";
 	}
 
 	@GetMapping("/about")
@@ -89,59 +131,52 @@ public class MyController {
 		return "admin";
 	}
 
-	
-	
-	
-	
 	// add new user
-//	@PostMapping("/registration")
-//	public ResponseEntity<Userdto> addUser(@Valid @RequestBody Userdto userDto)
-//	{
-//			Userdto addUserDto = this.userService.addUser(userDto);
-//			return new ResponseEntity<>(addUserDto,HttpStatus.CREATED);
-//	}
+	// @PostMapping("/registration")
+	// public ResponseEntity<Userdto> addUser(@Valid @RequestBody Userdto userDto)
+	// {
+	// Userdto addUserDto = this.userService.addUser(userDto);
+	// return new ResponseEntity<>(addUserDto,HttpStatus.CREATED);
+	// }
 
-//	
-//		// update existing user
-//		@PutMapping("/users/{userId}")
-//		public ResponseEntity<Userdto> updateUser(@Valid @RequestBody Userdto userDto,@PathVariable("userID") Long userId)
-//		{
-//			Userdto updatedUser =  this.userService.updateUser(userDto,userId);
-//			return ResponseEntity.ok(updatedUser);
-//		}
-//		
-//		
-//		
-//		// deleting the existing user
-//		@DeleteMapping("/users/{userId}")
-//		public ResponseEntity<ApiResponse> deleteUser(@PathVariable Long userId){
-//				this.userService.deleteUser(userId);
-//				return new ResponseEntity<ApiResponse>(new ApiResponse("User deleted successfully", true),HttpStatus.OK);
-//			
-//		}
-//		
-//		
-//		// get the list of users
-//		@GetMapping("/users")
-//		public ResponseEntity<List<Userdto>> getUsers()
-//		{ 
-//			 return ResponseEntity.ok(this.userService.getUsers());
-//		}
-//		
-//		
-//		
-//		// get single user through id
-//		@GetMapping("/users/{userId}")
-//		public ResponseEntity<Userdto> getUser(@PathVariable Long userId)
-//		{
-//			return ResponseEntity.ok(this.userService.getUser(userId));
-//		}
-//			
-	
+	// update existing user
+	@PutMapping("/users/{userId}")
+	public ResponseEntity<Userdto> updateUser(@Valid @RequestBody Userdto userDto,
+			@PathVariable("userID") Long userId) {
+		Userdto updatedUser = this.userService.updateUser(userDto, userId);
+		return ResponseEntity.ok(updatedUser);
+	}
+	//
+	//
+
+	// deleting the existing user
+	@DeleteMapping("/users/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser(@PathVariable Long userId) {
+		System.out.println("DELTED");
+		this.userService.deleteUser(userId);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("User deleted successfully", true), HttpStatus.OK);
+
+	}
+	//
+
+	// get the list of users
+	@GetMapping("/users")
+	public ResponseEntity<List<Userdto>> getUsers() {
+		return ResponseEntity.ok(this.userService.getUsers());
+	}
+	//
+	//
+
+	// get single user through id
+	@GetMapping("/users/{userId}")
+	public ResponseEntity<Userdto> getUser(@PathVariable Long userId) {
+		return ResponseEntity.ok(this.userService.getUser(userId));
+	}
+	//
+
 	@GetMapping("/users/{userId}/match-user")
-	public List<Userdto> getMatchingUsers(@PathVariable Long userId)
-	{
-		List<Userdto> user =this.userService.getMatchingUsers(userId);
-		return user;
+	public ResponseEntity<List<Userdto>> getMatchingUsers(@PathVariable Long userId) {
+		List<Userdto> user = this.userService.getMatchingUsers(userId);
+		return ResponseEntity.ok(user);
 	}
 }
